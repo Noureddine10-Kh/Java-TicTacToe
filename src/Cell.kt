4183e6a2497631f51f884ac460d1cc1d8a52c8d3
Khelifi Noureddine <khelifinoureddine10@gmail.com>
2021-04-07 11:50:06 +0100

    first step for the program start we need to add the expression .toRegex() for every regular expression found it.

---
+++ src/Cell.kt
@@ -0,0 +1,17 @@
+class Cell {
+    //This is one cell of the game grid
+    var contents = " "
+    var empty = true
+    fun output(): String {
+        return contents
+    }
+
+    fun placeMark() {
+        contents = if (TicTacToe.count % 2 == 0) {
+            "X"
+        } else {
+            "O"
+        }
+        empty = false
+    }
+}
